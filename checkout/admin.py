19c59483ebd64b9392e2e96373af26fd28b3f531
Fimber Uchenna Eze <uchenna631@gmail.com>
2023-02-19 22:32:43 +0000

    Add custom 404 error page

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from .models import Order, OrderLineItem
 
-
 class OrderLineItemAdminInline(admin.TabularInline):
     model = OrderLineItem
     readonly_fields = ('lineitem_total',)
