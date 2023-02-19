3e06883b4eff058dd4f928ed8a99dbb554ddce6d
Fimber Uchenna Eze <uchenna631@gmail.com>
2023-02-19 12:43:29 +0000

    Add host app

--- bigbrands/settings.py
+++ bigbrands/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['bigbrands.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['big-brands.herokuapp.com', 'localhost']
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
