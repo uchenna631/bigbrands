e5af00e69519710bb2a96f0b1242b91a8b2f4348
Fimber Uchenna Eze <uchenna631@gmail.com>
2023-02-19 23:00:05 +0000

    Add 404 error handler

--- bigbrands/urls.py
+++ bigbrands/urls.py
@@ -13,11 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from .views import handler404
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import handler404
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,4 +29,4 @@ urlpatterns = [
     path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-handler404 = 'boutique_ado.views.handler404'
+handler404 = 'bigbrands.views.handler404'
