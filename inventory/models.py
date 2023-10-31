2469f9e1c429193726d1762589360626892b4090
Fimber Eze <uchenna631@gmail.com>
2023-10-31 14:09:27 +0000

    Create an inventory and a discount model

---
+++ inventory/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from products.models import Product 
+
+
+class Inventory(models.Model):
+    product = models.OneToOneField(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=0)
+    created_on = models.DateTimeField(auto_now_add=True)
+    modified_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Inventory for {self.product.name}"
+
+
+class Discount(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    name = models.CharField(max_length=32)
+    discount_percent = models.DecimalField(max_digits=6, decimal_places=2)
+    active = models.BooleanField(default=False)
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+    def __str__(self):
+        return f"{self.discount_type} on {self.product.name}"
+
