4e66a7144b21efa0df56125e04d5cef7c899aa48
Fimber Eze <uchenna631@gmail.com>
2023-10-31 03:36:54 +0000

    Set collectstatic

--- bigbrands/settings.py
+++ bigbrands/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELPOMENT' in os.environ
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['big-brands.herokuapp.com', 'localhost', '127.0.0.1']
 
@@ -175,6 +175,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+
+COLLECTSTATIC = 1  # Automatically run collectstatic when running the development server
+
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
