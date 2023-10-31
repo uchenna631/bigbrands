c576f405dc1d37d575af94ddb10cdda7b1922209
Fimber Eze <uchenna631@gmail.com>
2023-10-31 14:22:33 +0000

    Set active field default to True

--- inventory/models.py
+++ inventory/models.py
@@ -16,7 +16,7 @@ class Discount(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     name = models.CharField(max_length=32)
     discount_percent = models.DecimalField(max_digits=6, decimal_places=2)
-    active = models.BooleanField(default=False)
+    active = models.BooleanField(default=True)
     start_date = models.DateField()
     end_date = models.DateField()
 
