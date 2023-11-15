b887ddc64248f3167d1d3343f23cefbec10da5e2
Fimber Uchenna Eze <uchenna631@gmail.com>
2023-11-15 03:36:25 +0000

    Ensure only logged in and authenticated superusers can accessproduct inventory

--- inventory/views.py
+++ inventory/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from products.models import Product
 from .models import Inventory
 from .forms import InventoryForm
 
 
+@login_required
 def inventory(request):
     """ A view to show all products, including sorting and search queries """
+
+     # Redirect unauthorised users to the home page
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
 
     inventories = Inventory.objects.all()
     context = {
@@ -14,10 +21,16 @@ def inventory(request):
     return render(request, 'inventory/inventory.html', context)
 
 
+@login_required
 def product_inventory(request, inventory_id):
     '''A view to show the inventory of a particular product.
      The product inventory could be updated as well'''
-     
+
+    # Redirect unauthorised users to the home page
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
     inventory = get_object_or_404(Inventory, pk=inventory_id)
     inventories = Inventory.objects.all()
 
